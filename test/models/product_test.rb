59a2258ed86147d6cb0cdf1ae48e85dc8b943c53
WilliamYoung <evaflamen@hotmail.com>
2015-01-08 20:17:06 -0800

    new file:   app/assets/javascripts/carts.coffee 	new file:   app/assets/javascripts/store.coffee 	deleted:    app/assets/stylesheets/application.css 	new file:   app/assets/stylesheets/application.css.scss 	new file:   app/assets/stylesheets/carts.scss 	new file:   app/assets/stylesheets/store.scss 	new file:   app/controllers/carts_controller.rb 	new file:   app/controllers/store_controller.rb 	new file:   app/helpers/carts_helper.rb 	new file:   app/helpers/store_helper.rb 	new file:   app/models/cart.rb 	modified:   app/models/product.rb 	new file:   app/views/carts/_form.html.erb 	new file:   app/views/carts/edit.html.erb 	new file:   app/views/carts/index.html.erb 	new file:   app/views/carts/index.json.jbuilder 	new file:   app/views/carts/new.html.erb 	new file:   app/views/carts/show.html.erb 	new file:   app/views/carts/show.json.jbuilder 	modified:   app/views/layouts/application.html.erb 	modified:   app/views/products/index.html.erb 	new file:   app/views/store/index.html.erb 	modified:   config/routes.rb 	new file:   db/migrate/20150109041546_create_carts.rb 	modified:   db/schema.rb 	new file:   test/controllers/carts_controller_test.rb 	new file:   test/controllers/store_controller_test.rb 	new file:   test/fixtures/carts.yml 	new file:   test/models/cart_test.rb 	modified:   test/models/product_test.rb

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,33 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  # product =Product.new(title: "My Book Title",
+  # 						description: "yyy",
+  # 						image_url: "zzz.jpg")
+  # product.price = -1
+  # assert
+ test "product attributes must mot be empty" do
+   product =Product.new
+   assert product.invalid?
+   assert product.errors[:title].any?
+   assert product.errors[:description].any?
+   assert product.errors[:price].any?
+   assert product.errors[:image_url].any?
+ end
+
+ test "product price must be positive" do
+   product =Product.new(title: "My Book Title",
+  						description: "yyy",
+  						image_url: "zzz.jpg")
+   product.price = -1
+   assert product.invalid?
+   assert_equal ["must be greater than or equal to 0.01"],
+     product.errors[:price]
+
+   product.price = 1
+   assert product.valid?
+  end
+
+  # test "image url" do
+  # 	Product.new
 end
