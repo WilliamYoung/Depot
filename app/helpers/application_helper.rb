e406eb458120db117b83d19a929097f4edcd041c
WilliamYoung <evaflamen@hotmail.com>
2015-01-14 00:42:51 -0800

    modified:   Gemfile 	modified:   Gemfile.lock 	modified:   app/assets/javascripts/application.js 	renamed:    app/assets/javascripts/store.coffee -> app/assets/javascripts/line_items.coffee 	new file:   app/assets/javascripts/store.js.coffee 	modified:   app/assets/stylesheets/application.css.scss 	new file:   app/assets/stylesheets/carts.css.scss 	renamed:    app/assets/stylesheets/carts.scss -> app/assets/stylesheets/line_items.scss 	modified:   app/assets/stylesheets/store.scss 	modified:   app/controllers/carts_controller.rb 	new file:   app/controllers/concerns/current_cart.rb 	new file:   app/controllers/line_items_controller.rb 	modified:   app/controllers/store_controller.rb 	modified:   app/helpers/application_helper.rb 	new file:   app/helpers/line_items_helper.rb 	modified:   app/models/cart.rb 	new file:   app/models/line_item.rb 	modified:   app/models/product.rb 	new file:   app/views/carts/_cart.html.erb 	modified:   app/views/carts/show.html.erb 	modified:   app/views/layouts/application.html.erb 	new file:   app/views/line_items/_form.html.erb 	new file:   app/views/line_items/_line_item.html.erb 	new file:   app/views/line_items/create.js.erb 	new file:   app/views/line_items/edit.html.erb 	new file:   app/views/line_items/index.html.erb 	new file:   app/views/line_items/index.json.jbuilder 	new file:   app/views/line_items/new.html.erb 	new file:   app/views/line_items/show.html.erb 	new file:   app/views/line_items/show.json.jbuilder 	modified:   app/views/store/index.html.erb 	modified:   config/routes.rb 	new file:   db/migrate/20150109051553_create_line_items.rb 	new file:   db/migrate/20150112073322_add_quantity_to_line_items.rb 	new file:   db/migrate/20150112074855_combine_items_in_cart.rb 	modified:   db/schema.rb 	new file:   test/controllers/line_items_controller_test.rb 	new file:   test/fixtures/line_items.yml 	new file:   test/models/line_item_test.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
-end
+	def hidden_div_if(condition, attributes = {}, &block)
+		if condition
+			attributes["style"] = "display: none"
+		end #condition
+		content_tag("div", attributes, &block)		
+	end #hidden_div_if
+end #ApplicationHelper
